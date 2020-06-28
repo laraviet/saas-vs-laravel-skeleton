bfaffdfc4ecd277e7be8ab0bd89d5fad0df921b6
Nguyen Trung Thanh <thanhnt@viralsoft.vn>
2020-06-28 21:15:02 +0700

    ACL

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::resource('roles','RoleController');
+    Route::resource('users','UserController');
+    Route::resource('products','ProductController');
+});
