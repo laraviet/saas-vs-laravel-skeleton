c5e593d1a90654bd06c6cdc7801ce2243ca903d3
Nguyen Trung Thanh <thanhnt@viralsoft.vn>
2020-07-05 09:50:42 +0700

    remove theme

--- themes/skote/common-components/functions/select2.blade.php
+++
@@ -1,6 +0,0 @@
-<script src="{{ theme_url('assets/libs/select2/select2.min.js')}}"></script>
-<script>
-    $(document).ready(function () {
-        $('.select2').select2();
-    });
-</script>
--- themes/skote/common-components/functions/toastr.blade.php
+++
@@ -1,7 +0,0 @@
-<script src="{{ theme_url('assets/libs/toastr/toastr.min.js')}}"></script>
-
-@if ($message = Session::get(config('core.session_success')))
-    <script>
-        toastr.success('{{ $message }}');
-    </script>
-@endif
