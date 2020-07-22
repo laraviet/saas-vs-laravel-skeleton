4498250fe09e838422fa8dbb81171dd3e310cfbc
Nguyen Trung Thanh <thanhnt@viralsoft.vn>
2020-07-22 18:20:33 +0700

    update theme

--- themes/skote/layouts/admin/hor-menu.blade.php
+++ themes/skote/layouts/admin/hor-menu.blade.php
@@ -8,7 +8,7 @@
                     @if(Module::find('adminhome') && Module::find('adminhome')->isEnabled())
                         <li class="nav-item">
                             <a class="nav-link arrow-none" href="{{ route('admin.home') }}" aria-expanded="false">
-                                <i class="bx bx-home-circle mr-2"></i>{{ __('core::labels.home') }}
+                                <i class="bx bx-home-circle mr-2"></i>{{ _t('home') }}
                             </a>
                         </li>
                     @endif
@@ -16,7 +16,7 @@
                     @if(Module::find('core') && Module::find('core')->isEnabled())
                         <li class="nav-item">
                             <a class="nav-link arrow-none" href="{{ route('users.index') }}" aria-expanded="false">
-                                <i class="bx bxs-user mr-2"></i>{{ __('core::labels.user') }}
+                                <i class="bx bxs-user mr-2"></i>{{ _t('user') }}
                             </a>
                         </li>
                     @endif
