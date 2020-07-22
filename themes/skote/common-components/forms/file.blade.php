df058b57ee96146ddc91357845a2958f953946da
Nguyen Trung Thanh <thanhnt@viralsoft.vn>
2020-07-22 08:38:22 +0700

    image preview

---
+++ themes/skote/common-components/forms/file.blade.php
@@ -0,0 +1,7 @@
+<div class="form-group row mb-4">
+    <label for="{{ $field }}"
+           class="col-form-label col-lg-2">{{ $label }}</label>
+    <div class="col-lg-10">
+        <input type="file" class="form-control" id="customFile" name="{{ $field }}">
+    </div>
+</div>
---
+++ themes/skote/common-components/forms/image-view.blade.php
@@ -0,0 +1,7 @@
+<div class="form-group row mb-4">
+    <label for=""
+           class="col-form-label col-lg-2"></label>
+    <div class="col-lg-10">
+        <img src="{{ url($path) }}"/>
+    </div>
+</div>
